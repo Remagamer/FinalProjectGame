c1e7d964dc4f7f8abf1ed8c3b113304f259b9a90
Unknown <Remagstudios@gmail.com>
2018-05-03 10:35:01 -0600

    who needs to pause
    when you have slomo?!?

--- Assets/Scripts/Pauser.cs
+++ Assets/Scripts/Pauser.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Pauser : MonoBehaviour {
 
+	private int winner = 0;
+
 	// Update is called once per frame
 	void Update () {
 		//this is the while loop to fill the quota.
 		if (Input.GetButton("Cancel"))
 		{
+			winner = 9000000;
+		}
+		while (winner>0)
+		{
+			winner-=1;
 		}
 	}
 }
