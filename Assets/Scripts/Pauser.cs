7cc3bebad7f20deebe8d1969a5634da1992292fd
Unknown <Remagstudios@gmail.com>
2018-05-03 10:23:21 -0600

    While loops are hard
    Come on lads

---
+++ Assets/Scripts/Pauser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pauser : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		//this is the while loop to fill the quota.
+		if (Input.GetButton("Cancel"))
+		{
+			while(true)
+			{
+				if (Input.GetButton("Cancel"))
+				{
+					break;
+				}
+			}
+		}
+	}
+}
