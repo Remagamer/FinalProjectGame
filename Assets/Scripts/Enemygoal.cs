4cf0d8c0e3aaac4dec5d9af452a442c3af67f519
Unknown <Remagstudios@gmail.com>
2018-05-03 21:25:28 -0600

    navmesh tutorials are out of date
    Nice }:]

---
+++ Assets/Scripts/Enemygoal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemygoal : MonoBehaviour {
+
+    private Transform goal;
+       
+    void Start () {
+          UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+          agent.destination = GameObject.Find("Player").GetComponent<Transform>().position; 
+	}
+
+	void Update () {
+		goal = GameObject.Find("Player").GetComponent<Transform>();
+	}
+}
