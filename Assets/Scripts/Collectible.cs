256da12972d78a936726e06b1ba2584de8a2d375
Unknown <Remagstudios@gmail.com>
2018-05-03 22:12:45 -0600

    I really did try my best.
    Unity doesn't make sense to me, and a lot of the ways we're doing things are undocumented which makes it hard to learn by doing. But I tried.

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//There's nothing more I can do, state machines are for animation and without the actual thing made in class I can't do anything to it :(
+
 public class Collectible : MonoBehaviour {
 
     public PlayerScript script;
